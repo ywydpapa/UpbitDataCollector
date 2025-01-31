79f6e107870fbc69bdc477ae20cad49ced13a934
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2025-01-31 15:10:51 +0900

    스케줄 변경

--- main.py
+++ main.py
@@ -111,8 +111,35 @@ def getWallet(uno):
             continue
     return mycoins
 
-schedule.every(30).minutes.do(runmain)
-schedule.every(60).minutes.do(runamt)
+schedule.every().day.at("01:00").do(runmain)
+schedule.every().day.at("02:00").do(runmain)
+schedule.every().day.at("03:00").do(runmain)
+schedule.every().day.at("04:00").do(runmain)
+schedule.every().day.at("05:00").do(runmain)
+schedule.every().day.at("06:00").do(runmain)
+schedule.every().day.at("07:00").do(runmain)
+schedule.every().day.at("08:00").do(runmain)
+schedule.every().day.at("09:00").do(runmain)
+schedule.every().day.at("10:00").do(runmain)
+schedule.every().day.at("11:00").do(runmain)
+schedule.every().day.at("12:00").do(runmain)
+schedule.every().day.at("13:00").do(runmain)
+schedule.every().day.at("14:00").do(runmain)
+schedule.every().day.at("15:00").do(runmain)
+schedule.every().day.at("16:00").do(runmain)
+schedule.every().day.at("17:00").do(runmain)
+schedule.every().day.at("18:00").do(runmain)
+schedule.every().day.at("19:00").do(runmain)
+schedule.every().day.at("20:00").do(runmain)
+schedule.every().day.at("21:00").do(runmain)
+schedule.every().day.at("22:00").do(runmain)
+schedule.every().day.at("23:00").do(runmain)
+schedule.every().day.at("00:00").do(runmain)
+
+schedule.every().day.at("00:30").do(runamt)
+schedule.every().day.at("06:30").do(runamt)
+schedule.every().day.at("12:30").do(runamt)
+schedule.every().day.at("18:30").do(runamt)
 
 
 while True:
